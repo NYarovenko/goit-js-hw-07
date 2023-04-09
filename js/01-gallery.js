696ce143e5f93fb5676301192a9ffb8156b3e115
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-04-09 23:55:19 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulContainer = document.querySelector('.gallery');
+
+let liItem = '';
+galleryItems.map(({ preview, original, description }) => {
+  liItem += `
+    <li class="gallery__item">
+         <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>
+    `;
+});
+ulContainer.innerHTML = liItem;
+
+let instance;
+ulContainer.addEventListener('click', onClick);
+function onClick(evt) {
+  evt.preventDefault();
+
+  instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height="600">`);
+  instance.show();
+  document.addEventListener('keydown', onKeyboardPress);
+}
+
+function onKeyboardPress(evt) {
+  console.log(evt.code);
+  if (evt.code === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', onKeyboardPress);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulContainer = document.querySelector('.gallery');
+
+let liItem = '';
+galleryItems.map(({ preview, original, description }) => {
+  liItem += `
+        <li class="gallery__item">
+             <a class="gallery__link" href="${original}">
+                 <img class="gallery__image" src="${preview}" alt="${description}"/>
+              </a>
+        </li>
+    `;
+});
+ulContainer.innerHTML = liItem;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
